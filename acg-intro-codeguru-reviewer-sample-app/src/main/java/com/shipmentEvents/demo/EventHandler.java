de452f57e7662e4d60d57af33abaf438b7f1e16a
Zhu <xiulei@08f8bc6ad7e6.ant.amazon.com>
2021-06-14 12:25:09 +0800

    update -2
    Signed-off-by: Zhu <xiulei@08f8bc6ad7e6.ant.amazon.com>

--- acg-intro-codeguru-reviewer-sample-app/src/main/java/com/shipmentEvents/demo/EventHandler.java
+++ acg-intro-codeguru-reviewer-sample-app/src/main/java/com/shipmentEvents/demo/EventHandler.java
@@ -48,7 +48,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
 
         final LambdaLogger logger = context.getLogger();
         try {
-            processShipmentUpdates(logger);
+            processShipmentUpdates(loggertest);
             return "SUCCESS";
         } catch (final Exception ex) {
             logger.log(String.format("Failed to process shipment Updates in %s due to %s", scheduledEvent.getAccount(), ex.getMessage()));
